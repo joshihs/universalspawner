b76c41a88a7c8c32120156146e1db9bdf6204d5a
DupeCreatorDev <vincejoshua0000@gmail.com>
2025-10-17 22:43:13 +0800

    Update sab-etc.lua

--- sab-etc.lua
+++ sab-etc.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/joshihs/universalspawner/refs/heads/main/sab-etc.lua"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DBQmy6kcafUllezt/raw"))()
 ‎
 ‎loadstring(game:HttpGet("https://raw.githubusercontent.com/namnnni2/Grow-a-garden/refs/heads/main/Petspamer.lua"))()

